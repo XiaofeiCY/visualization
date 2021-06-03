fc7a22b6c4b87982b408ad4d37fd3f942e5fa62e
春洋 <zhouchunyang@czb365.com>
2021-06-03 18:57:13 +0800

    绘制中心点的正方形

--- src/views/graph/graph-02.vue
+++ src/views/graph/graph-02.vue
@@ -1,20 +1,51 @@
 <template>
     <div class="graph-02">
-        <canvas></canvas>
+        <canvas width="512" height="512" class="canvas1"></canvas>
+        <canvas width="512" height="512" class="canvas2"></canvas>
     </div>
 </template>
 
 <script>
 export default {
+    mounted() {
+        this.ev_getCanvas1()
+        this.ev_getCanvas2()
+    },
+    methods: {
+        ev_getCanvas1() {
+            const canvas = document.querySelector('.canvas1');
+            const context = canvas.getContext('2d');
+            const rectSize = [100, 100];
+            context.fillStyle = 'red';
+            context.translate(-0.5 * rectSize[0], -0.5 * rectSize[1]);
+            context.beginPath();
+            context.rect(0.5 * canvas.width, 0.5 * canvas.height, ...rectSize);
+            context.fill();
+        },
+        ev_getCanvas2() {
+            const canvas = document.querySelector('.canvas2');
+            const context = canvas.getContext('2d');
+            const rectSize = [100, 100];
+            context.fillStyle = 'red';
+            context.save();
+            context.translate(-0.5 * rectSize[0], -0.5 * rectSize[1]);
+            context.beginPath();
+            context.rect(0.5 * canvas.width, 0.5 * canvas.height, ...rectSize);
+            context.fill();
+            context.restore();
+        }
+    }
 
 }
 </script>
 
 <style lang="less" scoped>
     .graph-02 {
-        canvas {
+        .canvas1, .canvas2 {
             width: 256px;
             height: 256px;
+            background-color: thistle;
+            margin-right: 10px;
         }
     }
 </style>
